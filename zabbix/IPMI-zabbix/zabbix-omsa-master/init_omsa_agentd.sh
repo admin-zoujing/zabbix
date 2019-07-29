6718691f79875d902a06c06638e16dad4fcf2638
Zou Jing <489548322@qq.com>
2019-07-29 20:36:39 +0800

    Update init_omsa_agentd.sh

--- zabbix/IPMI-zabbix/zabbix-omsa-master/init_omsa_agentd.sh
+++ zabbix/IPMI-zabbix/zabbix-omsa-master/init_omsa_agentd.sh
@@ -47,3 +47,34 @@ firewall-cmd --reload;
 
 #安装MegaCli软件包
 #yum -y install MegaCli-8.07.10-1.noarch.rpm
+
+
+
+#BIOS 界面设置 IPMI,重启F2进去System Setup界面,选择iDRAC Settings:进入后,先配置Network:先后在这个页面中配置了：启动iDrac网卡,设置idrac的ip,启用ipmi
+
+#1、yum install -y OpenIPMI ipmitool 
+# systemctl enable ipmi  && systemctl start ipmi    
+#2 使用ipmitool 也可以设置ipmi 的 ip（如果是裸机，就只能在BIOS 界面设置了）
+# ipmitool lan set 1 ipsrc static 
+# ipmitool lan set 1 ipaddr 10.11.1.99          
+# ipmitool lan set 1 netmask 255.255.0.0         
+# ipmitool lan set  1 defgw ipaddr 10.11.1.1   
+#3、查看ipmitool lan print 1
+#4、重启 ipmi 服务: systemctl restart ipmi
+#5、测试 ipmi
+#     ipmitool -I lanplus -H x.x.x.x  -U  root -P  password chassis power status
+#     DELL 默认的 ipmi 用户名密码是 root/calvin, 可以在BIOS user configuration 中修改，也可以通过 ipmitool 修改
+#    lanplus 可以替换为 lan 测试
+#    ipmitool user list 1           # 1 是channel ID
+#    ipmitool user set name 2 root   # 2 是用户的id
+#    ipmitool user set password 2 $newPassword
+#    ipmitool user enable 2
+#    ipmitool channel setaccess 1 2 callin=on ipmi=on link=on privilege=2
+#6 ipmitool lan print 1 
+#7、可以通过web 界面访问 DRAC 页面   https://$ipmi_IP
+#8.DELL 提供 win 界面的 ipmish.exe 和 ipmitool.exe (可以在官网下载)
+#9、ipmitool 常用命令
+#ipmitool user list 1         通道1用户列表 语法： list    [<channel number>]
+#ipmitool user priv 2 4 1     设置权限，语法:   priv     <user id> <privilege level> [<channel number>]
+#ipmitool lan set 1 access on 设置channel 1允许访问
+
